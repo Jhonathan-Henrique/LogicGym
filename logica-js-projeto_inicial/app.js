3ccf22348196088d6898fd35842a31ac25b4cb40
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-11-07 10:30:46 -0300

    chore: add operador ternario

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -9,7 +9,6 @@ console.log(chute)
 
 //Repetir enquanto chute for diferente do n. s
 while (chute != numeroSecreto) {
-
   chute = prompt('Escolha um numero de 1 a 10')
 
   if (numeroSecreto == chute) {
@@ -21,15 +20,16 @@ while (chute != numeroSecreto) {
       alert (`O numero secreto é maior que ${chute}`)
     }
   }
-
   tentativas++
-
 }
 
-if (tentativas > 1) {
+let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai, você acertou o número secreto! ${numeroSecreto} em tum total de ${tentativas} ${palavraTentativas}`);
+
+/* if (tentativas > 1) {
   alert(`Isso ai, você acertou o número secreto! ${numeroSecreto} em tum total de ${tentativas} tentativas`);
 } else {
   alert(`Isso ai, você acertou o número secreto! ${numeroSecreto} em tum total de ${tentativas} tentativa`);
 }
-
+*/ 
 
