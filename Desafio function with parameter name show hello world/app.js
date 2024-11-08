36fd8e732da60802d41b7a62932c0b28b963d19f
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-11-08 12:46:05 -0300

    add: challeger put name how parameter end show hello and name of person

---
+++ Desafio function with parameter name show hello world/app.js
@@ -0,0 +1,7 @@
+//Desafio: Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+
+function olaPessoa(nome){
+  console.log(`Olá! ${nome}`)
+}
+
+olaPessoa('Jhonathan');
