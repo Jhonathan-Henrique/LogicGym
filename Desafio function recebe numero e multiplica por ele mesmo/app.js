d3cde11228c7920450a707ad4d7326865bef3a72
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-11-08 13:16:59 -0300

    add: challenger double same number

---
+++ Desafio function recebe numero e multiplica por ele mesmo/app.js
@@ -0,0 +1,8 @@
+//Desafio: Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação
+//desse número por ele mesmo
+
+function dobraMesmoNumero(numero){
+  return numero * numero
+}
+
+console.log(dobraMesmoNumero(6))
