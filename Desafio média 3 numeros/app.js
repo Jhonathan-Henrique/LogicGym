b6ed58f210b26fe279f87bfde86d490628f15d39
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-11-08 13:06:31 -0300

    add: challenger calc 3 numbers get the media

---
+++ Desafio média 3 numeros/app.js
@@ -0,0 +1,7 @@
+//desafio: Criar uma função que recebe três números como parâmetros e retorna a média deles.
+
+function media(numero1, numero2, numero3){
+  return parseInt((numero1 + numero2 + numero3) / 3);
+}
+
+console.log(media(10,5,8))
